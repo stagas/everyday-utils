2f0b28d6bbd7f6acd2eb8b9ff372c678e9869cac
stagas <gstagas@gmail.com>
2022-07-30 01:15:27 +0300

    chore: fmt

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,7 +10,11 @@ module.exports = {
     ecmaVersion: 2021,
     sourceType: 'module',
   },
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'html-jsx'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'html-jsx',
+  ],
   ignorePatterns: ['dist', 'node_modules'],
   plugins: ['import'],
   rules: {
@@ -23,7 +27,10 @@ module.exports = {
     '@typescript-eslint/no-namespace': 'off',
     '@typescript-eslint/no-non-null-assertion': 'off',
     '@typescript-eslint/no-this-alias': 'off',
-    '@typescript-eslint/no-unused-vars': ['error', { args: 'all', argsIgnorePattern: '^_' }],
+    '@typescript-eslint/no-unused-vars': ['error', {
+      args: 'all',
+      argsIgnorePattern: '^_',
+    }],
     '@typescript-eslint/no-var-requires': 'off',
     'no-cond-assign': 'off',
     'no-empty': 'off',
